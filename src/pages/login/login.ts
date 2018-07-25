dfeece4e6f0295073eef1dcd85b31dec569fb40f
Marwan <marwan.ahmed87@outlook.com>
2018-07-25 08:46:18 +0300

    add-new.ts : add (if function to login btn ) show.scss : add padding tabs.html : add color to background tabs

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -13,6 +13,7 @@ import { HomePage } from '../home/home';
 })
 export class LoginPage {
 
+ 
   info = {
     email : '',
     password : ''
@@ -27,83 +28,41 @@ export class LoginPage {
   //   Password: ''
   // };
   isLogged = false;
-
+  
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public auth : AuthServiceProvider, public alertCtrl: AlertController) {
-
-    if (this.isLogged) {
-      navCtrl.setRoot(FamilyPage)
+      
+      if (this.isLogged) {
+        navCtrl.setRoot(FamilyPage)
+      }
+      
     }
+    ionViewDidLoad() {
+      console.log('ionViewDidLoad LoginPage');
+    }
+    
+    login() {
+      if (this.info.email == "" || this.info.password == "") {
+        const alert = this.alertCtrl.create({
+          title: 'تنبيه',
+          subTitle: 'يرجى ملئ جميع الحقول',
+          buttons: ['OK']
+        });
+        alert.present();
+      }
+      else {
+        this.auth.SignIn(this.info)
+        this.isLogged = true
+      }
 
+      // this.auth.SignIn(this.info)
+      // this.isLogged = true
+      // this.navCtrl.push(HomePage)
+ }
+
+    
+    
+    cnclBtn(){
+      this.navCtrl.push(HomePage)
+    }
   }
-
-
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
-
-login() {
-    this.auth.SignIn(this.info)
-    this.isLogged = true
-    this.navCtrl.push(FamilyPage)
-}
-
-
-  cnclBtn(){
-    this.navCtrl.push(HomePage)
-  }
-
-  // login() {
-  //   const prompt = this.alertCtrl.create({
-  //     title: 'تسجيل الدخول',
-  //     message: "ادخل اسمك و كلمة المرور لعرض معلومات الطالب",
-  //     inputs: [
-  //       {
-  //         name: 'Email',
-  //         placeholder: 'Email'
-  //       },
-  //       {
-  //         name: 'Password',
-  //         placeholder: 'password',
-  //         type: 'password'
-  //       },
-  //     ],
-  //     buttons: [
-  //       {
-  //         text: 'Cancel',
-  //         handler: data => {
-  //           console.log('Cancel clicked');
-  //         }
-  //       },
-  //       {
-  //         text: 'Login',
-  //         handler: data => {
-  //           this.info.email = data.Email;
-  //           this.info.password = data.Password;
-  //           this.auth.SignIn(this.info).then(nav => {
-  //             this.isLogged = true
-  //             this.navCtrl.push(FamilyPage)
-  //           console.log(nav);
-  //           // {
-  //           //   this.Kid.Email = nav.user.email,
-  //           //   this.Kid.Password = nav.user.uid,
-  //           //   this.Kid.FirstName = nav.user.displayName,
-  //           //   this.Kid.Address = nav.user.Address
-  //           // };
-  //           //console.log('email' + this.Kid.Email,'password' + this.Kid.Password + '----' + this.Kid.FirstName + 'aaa' + this.Kid.Address)
-  //           })
-  //         }
-  //       }
-  //     ]
-  //   });
-  //   prompt.present();
-  // }
-
-  // logOut(){
-  //   this.auth.signOut().then(() => (
-  //     this.navCtrl.setRoot(HomePage)
-  //   ))
-  // }
-
-}
